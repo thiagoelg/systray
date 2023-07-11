81a693e27a0b56e48a8ec323cfc96150e54c3fd7
Thiago Lugli <thiagoelg@gmail.com>
2023-07-11 11:30:26 -0300

    Change lib name

--- example/main.go
+++ example/main.go
@@ -5,8 +5,8 @@ import (
 	"io/ioutil"
 	"time"
 
-	"github.com/getlantern/systray"
-	"github.com/getlantern/systray/example/icon"
+	"github.com/thiagoelg/systray"
+	"github.com/thiagoelg/systray/example/icon"
 	"github.com/skratchdot/open-golang/open"
 )
 
